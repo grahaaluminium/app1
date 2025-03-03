0277ebf0fc76e370017dd59aafb32cf08763c004
grahaaluminium <grahaaluminiumperkasa@gmail.com>
2025-03-03 20:13:57 +0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -97,14 +97,14 @@ elif dropdown_dataSource == 'Yahoo Finance':
     st.write(start_year)
     
     ticker_data = data_yfinance_ticker
-    options = [stock for stock in ticker_data[dropdown_yahooExchange].dropna().tolist() if dt.datetime.strptime(stock.split(',')[1], '%Y%m%d').year < int(start_year)]
+    options = [stock for stock in ticker_data[dropdown_yahooExchange].tolist() if dt.datetime.strptime(stock.split(',')[1], '%Y%m%d').year < int(start_year)]
     # st.write(options)
     
     # Membuat multiple select
     if dropdown_yahooExchange == 'nasdaq':                
         if st.button('Choose Random Stocks'):
-            options = ticker_data[dropdown_yahooExchange].tolist()
-            st.write(options)
+            # options = ticker_data[dropdown_yahooExchange].tolist()
+            # st.write(options)
             st.session_state.yahoo_ticker = random.sample(options, 30)
         if 'yahoo_ticker' not in st.session_state:
             st.session_state.yahoo_ticker = []
