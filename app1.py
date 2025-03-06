d743cc76b105f4521b446e44cf3f39340d45d81e
grahaaluminium <grahaaluminiumperkasa@gmail.com>
2025-03-06 20:57:20 +0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -113,13 +113,14 @@ elif dropdown_dataSource == 'Alphavantage':
 
 if 'button_disabled' not in st.session_state:
     st.session_state.button_disabled = False
-    st.rerun()
+    # st.rerun()
 # Connect to QuantGenius AI Engine
 # createData_button = st.button("Create Test Data", disabled=st.session_state.button_disabled)
 
 if st.button("Create Test Data", disabled=st.session_state.button_disabled):
-    st.session_state.button_disabled = True
+    
     st.rerun()
+    st.session_state.button_disabled = True
     if dropdown_dataSource == 'Yahoo Finance' and len(yahoo_ticker) == 30:
         portfolio_data, portfolio_ticker = [], []
         for ticker in yahoo_ticker:
